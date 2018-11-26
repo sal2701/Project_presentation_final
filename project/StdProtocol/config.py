852b7f80c315995ec3e0c15b425b86fcdcc75dcd
sal2701 <42511766+sal2701@users.noreply.github.com>
2018-11-26 14:37:18 +0530

    Add files via upload

---
+++ project/StdProtocol/config.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, render_template, request, url_for
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
